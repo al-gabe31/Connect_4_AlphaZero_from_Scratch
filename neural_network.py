a0765fcb6632922f2c4e015b5fcba7d21694f560
Gabe Aquino <lq90575@umbc.edu>
2026-01-20 03:47:42 -0500

    added node_layer class

--- neural_network.py
+++ neural_network.py
@@ -12,6 +12,9 @@ def relu(value):
 def tanh(value):
     return math.tanh(value)
 
+def sigmoid(value):
+    return 1 / (1 + pow(math.e, -1 * value))
+
 class Node:
     def __init__(
             self, 
@@ -41,7 +44,7 @@ class Node:
     def __repr__(self):
         return f'{self.alias}: {self.value}'
 
-    def connect_preceding_nodes(self, node_list, weight_list):
+    def connect_preceding_nodes(self, node_list, weight_list: list[float]):
         for i in range(len(node_list)):
             # adding new connections for preceding nodes
             self.preceding_conns.append([node_list[i], weight_list[i]])
@@ -52,7 +55,7 @@ class Node:
         # don't forget to update value with the new connections
         self.value = self.calc_value()
 
-    def connect_suceeding_nodes(self, node_list, weight_list):
+    def connect_suceeding_nodes(self, node_list, weight_list: list[float]):
         for i in range(len(node_list)):
             # adding new connections for suceeding nodes
             self.suceeding_conns.append([node_list[i], weight_list[i]])
@@ -70,5 +73,52 @@ class Node:
         for i in range(len(self.preceding_conns)):
             summation += self.preceding_conns[i][0].value * self.preceding_conns[i][1]
 
-        # feed the summation into the activation function
-        return self.activation_function(summation)
+        # feed the summation into the activation function (don't forget to include the bias)
+        return self.activation_function(summation + self.bias)
+    
+
+
+class Node_Layer:
+    def __init__(
+            self,
+            node_list: list[Node] = None,
+            alias: str = '',
+            activation_function = lambda x: x,
+            bias_list: list[float] = None,
+        ):
+        if node_list is None:
+            self.node_list = []
+        else:
+            self.node_list = node_list # list of nodes in this layer
+        self.alias = alias
+
+        # going through each node in node_list and setting their activation function & bias
+        for i in range(len(node_list)):
+            node_list[i].activation_function = activation_function
+        if bias_list is not None:
+            for i in range(len(bias_list)):
+                node_list[i].bias = bias_list[i]
+
+    def __str__(self):
+        result = f'{self.alias}:\n'
+
+        for i in range(len(self.node_list)):
+            result += f'\t{str(self.node_list[i])}\n'
+
+        return result
+    
+    def __repr__(self):
+        result = f'Layer {self.alias} contain the following nodes:\n'
+
+        for i in range(len(self.node_list)):
+            result += f'\t{str(self.node_list[i])}'
+
+        return result
+    
+    def connect_preceding_layer(self, prev_layer, weight_matrix: list[list[float]]):
+        for i in range(len(weight_matrix)): # each row contains the weights for an object in this object's node_list
+            self.node_list[i].connect_preceding_nodes(prev_layer.node_list, weight_matrix[i])
+
+    def connect_suceeding_layer(self, suceeding_layer, weight_matrix: list[list[float]]):
+        for i in range(len(weight_matrix)): # each row contains the weights for an object in this object's node_list
+            self.node_list[i].connect_suceeding_nodes(suceeding_layer.node_list, weight_matrix[i])
