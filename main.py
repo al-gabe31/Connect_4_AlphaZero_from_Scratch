d54c1d239b120072fca38e208889a1b7f01b19a2
Gabe Aquino <lq90575@umbc.edu>
2026-01-20 01:13:41 -0500

    finished implementing basic class for node with connection methods

--- main.py
+++ main.py
@@ -1,1 +1,88 @@
 # this file is meant for testing code
+
+from neural_network import *
+
+activation_func = lambda x: 1 / (1 + pow(math.e, -1 * x))
+
+# # first layer of nodes
+# n11 = Node(0.35, 'n11')
+# n12 = Node(0.7, 'n12')
+
+# # second layer of nodes
+# n21 = Node(alias='n21', activation_function=activation_func)
+# n22 = Node(alias='n22', activation_function=activation_func)
+
+# # n21.connect_preceding_nodes([n11], [0.2])
+# # n21.connect_preceding_nodes([n12], [0.2])
+# # n22.connect_preceding_nodes([n11, n12], [0.3, 0.3])
+
+# n11.connect_suceeding_nodes([n21, n22], [0.2, 0.3])
+# n12.connect_suceeding_nodes([n21, n22], [0.2, 0.3])
+
+# # third layer of nodes
+# n31 = Node(alias='n31', activation_function=activation_func)
+
+# # n31.connect_preceding_nodes([n21, n22], [0.3, 0.9])
+
+# n21.connect_suceeding_nodes([n31], [0.3])
+# n22.connect_suceeding_nodes([n31], [0.9])
+
+
+
+# print(n11)
+# print(n12)
+# print(n21)
+# print(n22)
+# print(n31)
+
+# print(n11.preceding_conns)
+# print(n11.suceeding_conns)
+
+# print(n12.preceding_conns)
+# print(n12.suceeding_conns)
+
+# print(n21.preceding_conns)
+# print(n21.suceeding_conns)
+
+# print(n22.preceding_conns)
+# print(n22.suceeding_conns)
+
+# print(n31.preceding_conns)
+# print(n31.suceeding_conns)
+
+
+
+# 1st layer
+n11 = Node(0, 'n11')
+n12 = Node(1, 'n12')
+n13 = Node(1, 'n13')
+
+# 2nd layer
+n21 = Node(alias='n21')
+n22 = Node(alias='n22')
+n23 = Node(alias='n23')
+
+n21.connect_preceding_nodes([n11, n12, n13], [-2.2, -3.1, -4.6])
+n22.connect_preceding_nodes([n11, n12, n13], [3.3, 2.7, 4.3])
+n23.connect_preceding_nodes([n11, n12, n13], [-2.5, 4.0, 3.8])
+
+# 3rd layer
+n31 = Node(alias='n31')
+
+n31.connect_preceding_nodes([n21, n22, n23], [0, 2.7, -1.6])
+
+
+
+print(n11)
+print(n12)
+print(n13)
+print(n21)
+print(n22)
+print(n23)
+print(n31)
+
+
+
+
+
+print('ran without error')
--- neural_network.py
+++ neural_network.py
@@ -16,30 +16,59 @@ class Node:
     def __init__(
             self, 
             value: float = None,
-            preceding_conns = [],
-            suceeding_conns = [],
-            activation_function: function = lambda x: 0,
+            alias: str = '',
+            preceding_conns = None,
+            suceeding_conns = None,
+            activation_function = lambda x: x,
             bias: float = None,
-            preceding_conns_weights = [],
-            suceeding_conns_weights = [],
         ):
         self.value = value # value head of this node
-        self.preceding_conns = preceding_conns # list of nodes that connect to this object
-        self.suceeding_conns = suceeding_conns # list of nodes this object connects to
+        self.alias = alias # what this node will be called
+        if preceding_conns is None:
+            self.preceding_conns = []
+        else:
+            self.preceding_conns = preceding_conns # list of nodes that connect to this object and its corresponding weights
+        if suceeding_conns is None:
+            self.suceeding_conns = []
+        else:
+            self.suceeding_conns = suceeding_conns # list of nodes this object connects to and its corresponding weights
         self.activation_function = activation_function # activation funciton to achieve non-linearity
         self.bias = bias # bias term when calculating value head
-        self.preceding_conns_weights = preceding_conns_weights # list of weights from the nodes that connect to this object
-        self.suceeding_conns_weights = suceeding_conns_weights # list of weights this object connects to other nodes
 
+    def __str__(self):
+        return f'{self.alias}: {self.value}'
+    
+    def __repr__(self):
+        return f'{self.alias}: {self.value}'
+
+    def connect_preceding_nodes(self, node_list, weight_list):
+        for i in range(len(node_list)):
+            # adding new connections for preceding nodes
+            self.preceding_conns.append([node_list[i], weight_list[i]])
+
+            # making sure that connection is also updated for that preceding node
+            node_list[i].suceeding_conns.append([self, weight_list[i]])
+
+        # don't forget to update value with the new connections
+        self.value = self.calc_value()
+
+    def connect_suceeding_nodes(self, node_list, weight_list):
+        for i in range(len(node_list)):
+            # adding new connections for suceeding nodes
+            self.suceeding_conns.append([node_list[i], weight_list[i]])
+
+            # making sure that connection is also updated for that succeeding node
+            node_list[i].preceding_conns.append([self, weight_list[i]])
+
+            # don't forget to update value of the suceeding node
+            node_list[i].value = node_list[i].calc_value()
+        
     def calc_value(self):
         summation = 0
 
         # going through each precedding connection and add up: value * weight
         for i in range(len(self.preceding_conns)):
-            summation += self.preceding_conns[i].value * self.preceding_conns_weights[i]
+            summation += self.preceding_conns[i][0].value * self.preceding_conns[i][1]
 
         # feed the summation into the activation function
         return self.activation_function(summation)
-    
-    def update_weights(self):
-        pass
