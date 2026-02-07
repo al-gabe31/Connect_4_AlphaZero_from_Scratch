d9a4c4e8993937d13cc17ccf992a7f0bd6968bb7
Gabe Aquino <lq90575@umbc.edu>
2026-02-06 19:05:50 -0500

    batches 20/20 complete

--- mcts.py
+++ mcts.py
@@ -290,8 +290,8 @@ class MCTS_Tree:
         # randomly make the next move based on visit ratios and temperature
         transformed_probs = [curr_policies[i] if curr_policies[i] != 0 else -math.inf for i in range(len(curr_policies))] # masking 0 probabilities as negative infinity before passing into the softmax function
         num_pieces = len(curr_state) # number of pieces currently on the board
-        a_setting = 0.05
-        b_setting = 5
+        a_setting = 0.02
+        b_setting = 2.5
         move_probabilities: list[float] = [softmax(transformed_probs[i], transformed_probs, temperature=max(a_setting, pow(math.e, -1 * num_pieces / b_setting))) for i in range(len(transformed_probs))]
 
         # select a move given the probabilities above
